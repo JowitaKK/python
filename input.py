42bbbe46ad52d1e7e103501257093cd4aa2eafc4
JowitaKK <jowita.knap@wp.pl>
2019-07-31 14:28:49 +0100

    input and conditional checks

---
+++ input.py
@@ -0,0 +1,45 @@
+# print("What is your favorite colour?")
+# data = input()
+# print("You said " + data)
+
+# conditional check
+
+name = "Jow"
+
+if name == "Tomek":
+    print("its not correct")
+elif name == "Jow":
+    print("Almost right")
+else:
+    print("carry on")
+
+# exercise
+# from random import randint
+# 1)
+
+choice = randint(1, 10)
+
+if choice == 7:
+    print("lucky")
+else:
+    print("unlucky")
+
+# 2)
+from random import randint
+num = randint(1, 1000)
+
+if num % 2 != 0:
+    print("odd")
+else:
+    print("even")
+
+# 3) to check if user put any input
+
+animal = input("entern animal")
+if animal:  # here is no input so that is how we know it value is false so is checking rest
+    print(animal + "is my favourite too")
+else:
+    print("You did not say anything!!!")
+
+
+
