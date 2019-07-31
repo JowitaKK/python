bf335392084549a482c06fdcb78a433c31d9632b
JowitaKK <jowita.knap@wp.pl>
2019-07-31 10:47:28 +0100

    first exercises

---
+++ hello_world.py
@@ -0,0 +1,49 @@
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print (1/3) # to make comments #
+
+new_line = "hello \n word"
+print(new_line)
+
+
+# concatenation
+
+str_one ="yours"
+str_two = "face"
+
+str_three = str_one + " "  + str_two
+
+name = "colt"
+name += "steele"  # its add colt steele
+
+people = 1
+people += 99
+
+people = 100
+people -= 1
+
+# strings and int
+
+guess = 8
+print(f"Your guess of {guess + 1} was incorrect!")
+
+# or
+fruit = "apple"
+vegetables = "carrot"
+print(" You need to buy {} and {} ".format(fruit, vegetables))
+
+first = "Jowita"
+last = "Knap"
+
+print("My is {} and surname is {}".format(first, last))
+
+
+# converting data types
+
+decimal = 12.4556
+integer = int(decimal)  # 12
+
+my_list = [1, 2, 3]
+my_list_as_string = str(my_list)   # "[1,2,3]"
