c43fcb6452644988085e95ae1cbfeed3cd1e74e8
JowitaKK <jowita.knap@wp.pl>
2019-08-04 11:37:14 +0100

    comparisons, if statement

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
-num1 = input ("Enter a number :")
-num2 = input ("Enter another number :")
-result = int(num1) + int(num2)    # or float(num1) + float(num2)  prints decimal num
+# num1 = input ("Enter a number :")
+# num2 = input ("Enter another number :")
+# result = int(num1) + int(num2)    # or float(num1) + float(num2)  prints decimal num
+#
+# print(result)
 
-print(result)
+
 
 
---
+++ function_if_statement.py
@@ -0,0 +1,42 @@
+def say_hi(name, age):
+    print("Hello " + name + " you are " + str(age))
+
+print("Top")
+say_hi("Jov", 35)
+print("Button ")
+
+
+#=========return function =============
+
+def cube(num):
+    return num*num*num
+
+
+result = cube(4)
+print(result)
+
+
+#============if statement=============
+
+is_male = True
+is_tall = True
+
+if is_male
+    print("You are a tall male")
+elif is_male and not (is_tall):
+    print("You are not a male but are tall")
+else:
+    print("You are not a tall man ")
+
+#===========comparisons================
+
+def max_num(num1, num2, num3)
+    if num1 >= num2 and num1 >= num3:    # if "dog" == "dog"
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+print(max_num(20, 230, 3))
+
+
