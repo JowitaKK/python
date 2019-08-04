352ac961061dd94b71809907d38fde12243ab867
JowitaKK <jowita.knap@wp.pl>
2019-08-04 17:59:15 +0100

    starting init class

---
+++ class_object.py
@@ -0,0 +1,21 @@
+Student.py  # create a class
+
+class Student :
+
+    def __init__(self, name, major, gpa. is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa - gpa
+        self.is_on_probation = is_on_probation
+
+
+#==================================next class like app.py
+
+# from Student import Student    // from Studen file import Student class
+
+Student1 = Studemy("Jim", "Business", 3.1, False)   # student object
+Student2 = Studemy("Pam", "Art", 3.1, True)   # student object
+
+
+print(student1.name)
+
