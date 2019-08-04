bdfde8860bed30a2a6bc76def91da2c85dcf7d1b
JowitaKK <jowita.knap@wp.pl>
2019-08-04 12:02:41 +0100

    calculation modification

--- calculator.py
+++ calculator.py
@@ -4,6 +4,20 @@
 #
 # print(result)
 
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")    # + -  / *
+num2 = float(input("Enter second number: "))
 
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-" :
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*"
+    print(num1 * num2)
+else:
+    print("Invalid operator")
 
 
